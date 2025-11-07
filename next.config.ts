f8e76d006e11f94cd8070b456cff53d8588dccc3
CanopusFalling <kent_sam@outlook.com>
2025-11-07 07:07:10 +0000

    Removed experimental live data cloudflare binding.

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,4 @@ export default nextConfig;
 
 // added by create cloudflare to enable calling `getCloudflareContext()` in `next dev`
 import { initOpenNextCloudflareForDev } from "@opennextjs/cloudflare";
-initOpenNextCloudflareForDev({
-  experimental: { remoteBindings: true },
-});
+initOpenNextCloudflareForDev();
