8e2f4921c613fbc3a20865b5918298095f5eab27
CanopusFalling <kent_sam@outlook.com>
2025-11-09 05:19:28 +0000

    Updated look a little bit, will need to start actually making the useful pages next.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import Link from "next/link";
 
 export const metadata: Metadata = {
   title: "OSM Address Audit",
@@ -13,12 +14,19 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body>
-        {children}
-        <footer>
+    <html className="h-full" lang="en">
+      <body className="flex flex-col h-full">
+        <main className="grow">{children}</main>
+        <footer className="bg-gray-500/20 mt-20 px-8 py-4">
           Contains public sector information licensed under the Open Government
-          Licence v3.0
+          Licence v3.0 | Thanks to{" "}
+          <Link
+            className="text-blue-500 underline"
+            href="https://www.openstreetmap.org/user/CjMalone"
+          >
+            CJ Malone
+          </Link>{" "}
+          for geo-coding the data!
         </footer>
       </body>
     </html>
