5439590372f8ec847d180ad450725bc8da97e1bf
CanopusFalling <kent_sam@outlook.com>
2025-11-10 01:04:34 +0000

    Fix build error.

--- app/components/MarkerMap.tsx
+++ app/components/MarkerMap.tsx
@@ -8,12 +8,13 @@ import "leaflet/dist/leaflet.css";
 import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png";
 import markerIcon from "leaflet/dist/images/marker-icon.png";
 import markerShadow from "leaflet/dist/images/marker-shadow.png";
+import { StaticImageData } from "next/image";
 
 // //@ts-expect-error required to delete leaflet icon url
 // delete L.Icon.Default.prototype._getIconUrl; // Remove the old URL
 
 // Required as the nextjs deployment behaves slightly differently on cloudflare workers vs dev.
-const getIconUrl = (asset: any) => {
+const getIconUrl = (asset: StaticImageData | string) => {
   if (typeof asset === "object" && asset.src) {
     return asset.src;
   }
