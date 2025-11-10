b0ae1070ac6e3bd913ffad47f443c55e38359634
CanopusFalling <kent_sam@outlook.com>
2025-11-10 00:43:21 +0000

    Attempting some changes to see if they work on the server.

--- app/components/MarkerMap.tsx
+++ app/components/MarkerMap.tsx
@@ -2,16 +2,18 @@
 
 import L from "leaflet";
 import { MapContainer, Marker, TileLayer, Popup } from "react-leaflet";
+
 import "leaflet/dist/leaflet.css";
+
 import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png";
 import markerIcon from "leaflet/dist/images/marker-icon.png";
 import markerShadow from "leaflet/dist/images/marker-shadow.png";
 
-//@ts-expect-error required to delete leaflet icon url
-delete L.Icon.Default.prototype._getIconUrl; // Remove the old URL
+// //@ts-expect-error required to delete leaflet icon url
+// delete L.Icon.Default.prototype._getIconUrl; // Remove the old URL
 
 L.Icon.Default.mergeOptions({
-  iconUrl: markerIcon, // No need for .src
+  iconUrl: markerIcon.src,
   iconRetinaUrl: markerIcon2x,
   shadowUrl: markerShadow,
 });
