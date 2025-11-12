fe4713f37c1d7161611101aac49f3ee3c9874eb3
CanopusFalling <kent_sam@outlook.com>
2025-11-12 13:45:40 +0000

    Force dynamic across the whole application temporarily.

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,8 @@ export const metadata: Metadata = {
     "Auditing addresses in the OSM database using data published by local authorities.",
 };
 
+export const dynamic = "force-dynamic";
+
 export default function RootLayout({
   children,
 }: Readonly<{
